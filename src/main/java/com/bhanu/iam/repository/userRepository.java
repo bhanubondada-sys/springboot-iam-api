6caa135519ef41a3debbafec92e21d3fdf852d00
Bhanu Bondada | Java Full Stack Developer <bhanujavadeveloper25@gmail.com>
2026-01-21 11:33:55 -0500

    Create userRepository.java

---
+++ src/main/java/com/bhanu/iam/repository/userRepository.java
@@ -0,0 +1,10 @@
+package com.bhanu.iam.repository;
+
+import com.bhanu.iam.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
